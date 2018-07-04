1e9e33a5c824e3ebfc21dbc98b66e1ab8b551108
Ivan Zelenskyi <izelensk@e1r9p4.unit.ua>
2018-07-04 21:29:29 +0300

    values parsed

--- main.cpp
+++ main.cpp
@@ -18,12 +18,14 @@ bool	isupper_s(std::string s)
 	return true;
 }
 
-bool	isduplicated(std::vector<Fact> vec, std::string name)
+bool	isduplicated(std::vector<Fact *> vec, std::string name)
 {
 	for (int i = 0; i < vec.size(); i++)
 	{
-		if (vec[i].getName() == name)
+		if (vec[i]->getName() == name)
+		{
 			return true;
+		}
 	}
 	return false;
 }
@@ -37,9 +39,9 @@ int main(int argc, char const *argv[])
 		exit(0);
 	}
 	
-	std::vector<Fact> facts;
-	std::vector<Fact> facts_defined;
-	std::vector<Fact> facts_undefined;
+	std::vector<Fact *> facts;
+	std::vector<Fact *> facts_defined;
+	std::vector<Fact *> facts_undefined;
 
 	std::ifstream infile(argv[1]);
 	std::string line;
@@ -53,10 +55,26 @@ int main(int argc, char const *argv[])
 			iss >> sym;
 			if (sym[0] == '#')
 				break ;
-			if (isupper_s(sym) && !isduplicated(facts, sym)) //isdup not working
+			else if (isupper_s(sym) && !isduplicated(facts, sym))
 			{
-				std::cout << "symtring: " << sym << std::endl;
-				facts.push_back(Fact(-1, sym));
+				std::cout << "Syms: " << sym << std::endl;
+				facts.push_back(new Fact(-1, sym));
+			}
+			else if (sym[0] == '=' && isalpha(sym[1]) && isupper(sym[1]) && isduplicated(facts, sym))
+			{
+				for (int i = 1; i < sym.length(); i++)
+				{
+					facts_defined.push_back(new Fact(1, std::string(1, sym[i])));
+					std::cout << " = " << facts_defined[facts_defined.size() - 1]->getName() << std::endl;
+				}
+			}
+			else if (sym[0] == '?' && isalpha(sym[1]) && isupper(sym[1]) && isduplicated(facts, sym))
+			{
+				for (int i = 1; i < sym.length(); i++)
+				{
+					facts_undefined.push_back(new Fact(1, std::string(1, sym[i])));
+					std::cout << " ? " << facts_undefined[facts_undefined.size() - 1]->getName() << std::endl;
+				}
 			}
 		}
 	}
